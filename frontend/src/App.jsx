a2e8db44a09c3719a5c2b84b84864915a2d75bd5
ashleyelw <ashleyelworthy@gmail.com>
2026-03-27 22:11:37 +1100

    Expanding site
    deleted the html file i created because there was already an index html. Added some more category flexbox containers and experimented with grid container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,14 +13,38 @@ function App() {
                   </div>
               </div>
           </div>
-          
-          <p> Choose a fruit: </p>
-            <div class="fruit">
-              <div>Apple</div>
-              <div>Banana</div>
-              <div>Cherry</div>
-            </div>
-      </div>
+    
+    <h1>Grocery Products</h1>
+
+    <h2>Fruits</h2> 
+      <div class="fruit">
+      <div>Apple</div>
+      <div>Banana</div>
+      <div>Cherry</div>
+    </div>
+
+    <h2> Vegetables</h2>
+      <div class="vegetable">
+      <div>Carrot</div>
+      <div>Broccoli</div>
+      <div>Spinach</div>
+    </div>
+
+
+    <h2>Dairy</h2>
+     <div class="dairy">
+      <div>Milk</div>
+      <div>Cheese</div>
+      <div>Yogurt</div>
+    </div>
+    </div>
+  
+    <div class="grid-container">
+      <div class="item1">1</div>
+      <div class="item2">2</div>
+      <div class="item3">3</div>  
+    </div>
+
     </>
   );
 }
