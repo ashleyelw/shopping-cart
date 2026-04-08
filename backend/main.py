c579212bef9e720abb97091c310508060ce0c384
ashleyelw <ashleyelworthy@gmail.com>
2026-04-08 11:18:51 +1000

    connected to sql database
    i think its correct...

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+import pymysql
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"Hello": "World"}
+@app.get("/products")
+def get_products():
+    return products
+
+with pymysql.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="shoppingcartdb",
+) as connection:
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM products")
+        products = cursor.fetchall()
+        print(products)
